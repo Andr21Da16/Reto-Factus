af00db54294f4d9757837bd4e3e1071cf3077edd
johel <johelandreevargasvillanueva07@gmail.com>
2026-03-01 14:46:14 -0500

    fix create user and rol enititie

---
+++ Factus-Backend/src/main/java/pe/andree/retofactusbackend/entities/Rol.java
@@ -0,0 +1,25 @@
+package pe.andree.retofactusbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name_rol")
+    private String nameRol;
+
+    @OneToMany(mappedBy = "rol", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<User> users;
+
+
+}
---
+++ Factus-Backend/src/main/java/pe/andree/retofactusbackend/entities/User.java
@@ -0,0 +1,35 @@
+package pe.andree.retofactusbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 200)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 250)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, length = 200)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "phone", nullable = false, length = 20)
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Rol rol;
+
+}
