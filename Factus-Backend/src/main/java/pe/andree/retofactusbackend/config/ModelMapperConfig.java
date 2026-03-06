4116adf5f0af5fe5eda7f9f924f428f932770021
johel <johelandreevargasvillanueva07@gmail.com>
2026-03-06 14:08:57 -0500

    feat: add company tables and app settings per company; implement role DTOs and controller to allow creating roles; add company DTOs and mapper

--- Factus-Backend/src/main/java/pe/andree/retofactusbackend/config/ModelMapperConfig.java
+++ Factus-Backend/src/main/java/pe/andree/retofactusbackend/config/ModelMapperConfig.java
@@ -1,6 +1,7 @@
 package pe.andree.retofactusbackend.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pe.andree.retofactusbackend.dto.response.auth.UserProfileResponseDTO;
@@ -24,6 +25,9 @@ public class ModelMapperConfig {
         mapper.typeMap(User.class, UserProfileResponseDTO.class).addMappings(m ->
                 m.map(src -> src.getCompany().getNameCompany(), UserProfileResponseDTO::setCompany)
         );
+
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
 
         return mapper;
     }
