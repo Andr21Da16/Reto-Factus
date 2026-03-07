581640ac38598de2ab7a67354fd24c4605c8655b
johel <johelandreevargasvillanueva07@gmail.com>
2026-03-06 20:56:44 -0500

    feat: integrate Cloudflare R2 and add test file upload for company settings

--- Factus-Backend/src/main/java/pe/andree/retofactusbackend/service/impl/AppSettingsServiceImpl.java
+++ Factus-Backend/src/main/java/pe/andree/retofactusbackend/service/impl/AppSettingsServiceImpl.java
@@ -2,15 +2,19 @@ package pe.andree.retofactusbackend.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 import pe.andree.retofactusbackend.domain.entities.AppSetting;
+import pe.andree.retofactusbackend.domain.entities.Company;
 import pe.andree.retofactusbackend.domain.setting.AppSettingsData;
 import pe.andree.retofactusbackend.dto.ApiResponse;
 import pe.andree.retofactusbackend.dto.request.settings.AppSettingRequestDTO;
 import pe.andree.retofactusbackend.dto.response.setting.AppSettingResponseDTO;
 import pe.andree.retofactusbackend.mapper.AppSettingMapper;
 import pe.andree.retofactusbackend.repository.AppSettingRepository;
+import pe.andree.retofactusbackend.repository.CompanyRepository;
 import pe.andree.retofactusbackend.service.AppSettingService;
 
 import java.time.LocalDateTime;
@@ -20,6 +24,7 @@ import java.time.LocalDateTime;
 public class AppSettingsServiceImpl implements AppSettingService {
 
     private final AppSettingRepository appSettingRepository;
+    private final CompanyRepository companyRepository;
 
     private final AppSettingMapper appSettingMapper;
 
@@ -34,6 +39,10 @@ public class AppSettingsServiceImpl implements AppSettingService {
             throw new DataIntegrityViolationException("Adjustments have already been made for that company.");
         }
 
+        Company company = companyRepository.findById(settings.getCompanyId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found"));
+
+
         String imgUrl = fileUploadService.uploadFile(file, settings.getCompanyId());
 
         AppSetting result = appSettingMapper.toAppSetting(settings);
@@ -43,7 +52,7 @@ public class AppSettingsServiceImpl implements AppSettingService {
         }
 
         result.getSettings().getBrandingSettings().setLogoUrl(imgUrl);
-
+        result.setCompany(company);
         result = appSettingRepository.save(result);
 
         return ApiResponse.<AppSettingResponseDTO>builder()
